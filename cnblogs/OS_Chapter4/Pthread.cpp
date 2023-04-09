5213fa86f4ac2add199cdd73207b5333b7050a6c
ly <1943352780@qq.com>
2023-04-09 21:42:06 +0800

    OS

--- cnblogs/OS_Chapter4/Pthread.cpp
+++ cnblogs/OS_Chapter4/Pthread.cpp
@@ -1,58 +1,97 @@
 
-#include <cstdio>
+
+#include <iostream>
 #include <thread>
 #include <mutex>
 using namespace std;
-#include <windows.h>
-#include <easyx.h>
+#include <unistd.h>
 
+int tickets = 1000;
+mutex mtx;
 
-void Rotate()//旋转
+void start_routine(const char* name)
 {
-    settextcolor(WHITE);
-    char buffer[4] = { '|','/','-','\\' };//注意转义字符
-
-    int index = 0;
-    while (true)
+    while(true)
     {
-        char puts[2] = { 0 };
-        puts[0] = buffer[index++];
-        outtextxy(10, 10, puts);
-        index %= 4;
-        Sleep(100);
-    }
-
-}
-
-void Progress()//进度条
-{
-    while (true)
-    {
-        char buffer[102] = { 0 };
-        int cnt = 0;
-        while (cnt <= 100)
+        mtx.lock();
+        if(tickets > 0)//如果票还够
         {
-            outtextxy(10, 40, buffer);
-            fflush(stdout);
-            buffer[cnt++] = '#';
-            Sleep(50);
+            cout << name << " get ticket:" << tickets-- << endl;
+            mtx.unlock();
+            usleep(1234);//休眠放在这里就会让当前线程发生切换，因为锁已经释放了，其他线程就能进入临界区
         }
-        cleardevice();
+        else 
+        {
+            mtx.unlock();
+            break;
+        }
     }
-
 }
 
 int main()
 {
-    initgraph(1024, 480);
-    /*   Rotate();
-       Progress();*/
-    thread t1(Rotate);
-    //Sleep(2);
-    thread t2(Progress);
-
+    thread t1(start_routine,"pthread 1");
+    thread t2(start_routine,"pthread 2");
     t1.join();
     t2.join();
-    while (true);
     return 0;
 }
+
+
+// #include <cstdio>
+// #include <thread>
+// #include <mutex>
+// using namespace std;
+// #include <windows.h>
+// #include <easyx.h>
+
+
+// void Rotate()//旋转
+// {
+//     settextcolor(WHITE);
+//     char buffer[4] = { '|','/','-','\\' };//注意转义字符
+
+//     int index = 0;
+//     while (true)
+//     {
+//         char puts[2] = { 0 };
+//         puts[0] = buffer[index++];
+//         outtextxy(10, 10, puts);
+//         index %= 4;
+//         Sleep(100);
+//     }
+
+// }
+
+// void Progress()//进度条
+// {
+//     while (true)
+//     {
+//         char buffer[102] = { 0 };
+//         int cnt = 0;
+//         while (cnt <= 100)
+//         {
+//             outtextxy(10, 40, buffer);
+//             fflush(stdout);
+//             buffer[cnt++] = '#';
+//             Sleep(50);
+//         }
+//         cleardevice();
+//     }
+
+// }
+
+// int main()
+// {
+//     initgraph(1024, 480);
+//     /*   Rotate();
+//        Progress();*/
+//     thread t1(Rotate);
+//     //Sleep(2);
+//     thread t2(Progress);
+
+//     t1.join();
+//     t2.join();
+//     while (true);
+//     return 0;
+// }
